d249783991df60cc09d928196f77fb1f7b55ad48
rdixi4 <rdixit5@sapient.com>
2020-01-08 17:38:12 +0530

    adding services

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home() {
-    return <div>Welcome raghvendra ji to next.js!</div>
-  }
-  
-  export default Home
+import fetch from 'isomorphic-unfetch';
+import Products from '../component/Products';
+
+const Index = (props) => (
+      <Products products={(props && props.products) || [] } />
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://my-json-server.typicode.com/mood-board/demo/products');
+  const data = await res.json();
+  return {
+    products: data
+  };
+};
+export default Index
